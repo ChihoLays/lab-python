14c82223095a4036e091c20016b6655f448507da
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2020-11-07 14:46:49 +0700

    Update HM11.py

--- HM11.py
+++ HM11.py
@@ -61,4 +61,76 @@ print(f"Current time: {test.get_time()}")
 test.setTime(23,56,59)
 test.run()
 
-#Q2
+#Q2 base Q
+import turtle
+def RobotBattle():
+    # robotList stores the list of robots in the battle
+    robotList = []
+
+    while True:
+        #Clear the screen and draw the robots
+        turtle.clear()
+        for robot in robotList:
+            robot.draw()
+        #Display the satatus of each robot
+        print("==== Robots ====")
+        i=0
+        for robot in robotList:
+            print(f"{i} : ")
+            robot.displayStatus()
+            i+1
+        print("===============")
+
+        #Ask which robot to command or to create a new robot
+        choice = input("Enter which robot to order, 'c' to create new robot, 'q' to quit")
+        if choice == "q":
+            break
+        elif choice == "c":
+            print("Enter which type of robot to create")
+            robotType = input("'r' for Robot, 'm' for MedicBot, 's' for StrikerBot")
+            if robotType == "r":
+                newRobot = Robot()
+            elif robotType == "m":
+                newRobot = MedicBot()
+            elif robotType == "s":
+                newRobot = StrikerBot()
+            robotList = robotList + [newRobot]
+        else:
+            n = int(choice)
+            robotList[n].command(robotList)
+        #Delete all the robots with health <= 0 from the list
+        i = 0
+        for robot in robotList:
+            if(robot.health <= 0):
+                del robotList[i]
+            i += 1
+
+class Robot(object):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.health = 100
+        self.energy = 100
+
+    def move(self,x,y):
+    # Your code
+
+    def draw(self):
+    # Your code
+
+    def displayStatus(self):
+        print(f"x= {self.x} y= {self.y} health= {self.health} energy= {self.energy}")
+
+    def command(self,robotList):
+        print("Possible action: move")
+        newX = int(input("Enter new x-coordinate: "))
+        newY = int(input("Enter new y-coordinate: "))
+        self.move(newX,newY)
+
+class MedicBot(Robot):
+    # Your code
+
+class StrikerBot(Robot):
+    # Your code
+   
+#Q2 already code
