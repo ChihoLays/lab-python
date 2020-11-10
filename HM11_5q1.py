f54db8ed1c8ad0b506317945484767ea0342c800
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2020-11-10 14:04:17 +0700

    Update HM11_5q1.py

--- HM11_5q1.py
+++ HM11_5q1.py
@@ -1,8 +1,8 @@
 import abc
 from turtle import *
 class Char(abc.ABC):
-    def __init__(self,width):
-        self.width = width
+    def __init__(self):
+        self.width = 20
     def draw(self,x,y):
         pu()
         goto(x,y)
@@ -13,8 +13,8 @@ class Char(abc.ABC):
         return self.width
 
 class Char0(Char):
-    def __init__(self,width):
-        super().__init__(width)
+    def __init__(self):
+        super().__init__()
     def draw(self,x,y):
         super().draw(x,y)
         for i in range(2):
@@ -24,8 +24,6 @@ class Char0(Char):
             lt(90)
 
 class Char1(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         fd(self.width)
@@ -36,8 +34,6 @@ class Char1(Char):
         fd(self.width / 1.5)
 
 class Char2(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         fd(self.width)
@@ -52,8 +48,6 @@ class Char2(Char):
         fd(self.width)
 
 class Char3(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         fd(self.width)
@@ -68,8 +62,6 @@ class Char3(Char):
         fd(self.width)
 
 class Char4(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x+self.width,y)
         lt(90)
@@ -81,8 +73,6 @@ class Char4(Char):
         fd((self.width*1.5)/2)
 
 class Char5(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         fd(self.width)
@@ -96,8 +86,6 @@ class Char5(Char):
         fd(self.width)
 
 class Char6(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         for i in range(2):
@@ -111,16 +99,12 @@ class Char6(Char):
         fd(self.width)
 
 class Char7(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y+(self.width*1.5))
         fd(self.width)
         goto(x,y)
 
 class Char8(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x,y)
         for i in range(4):
@@ -132,8 +116,6 @@ class Char8(Char):
             lt(90)
 
 class Char9(Char):
-    def __init__(self,width):
-        super().__init__(width)
     def draw(self,x,y):
         super().draw(x+(self.width),y)
         goto(x+(self.width),y+(self.width * 1.5) / 2)
@@ -144,20 +126,18 @@ class Char9(Char):
             fd((self.width * 1.5) / 2)
             lt(90)
 
-def drawNum(usnum,size):
+def drawNum(usnum):
     ht()
     speed(0)
     usnum = str(usnum)
     posX = 0
-    num = {"0": Char0(size), "1": Char1(size), "2": Char2(size), "3": Char3(size),
-           "4":Char4(size), "5": Char5(size),"6": Char6(size), "7": Char7(size),
-           "8": Char8(size), "9": Char9(size)}
+    num = {"0": Char0(), "1": Char1(), "2": Char2(), "3": Char3(),
+           "4":Char4(), "5": Char5(),"6": Char6(), "7": Char7(),
+           "8": Char8(), "9": Char9()}
     for char in usnum:
         if char in num:
             num[char].draw(posX, 0)
-            posX += size * 1.25
+            posX += 20 * 1.25
     done()
 
-
-
-drawNum(123456789,20)
+drawNum(123456789)
