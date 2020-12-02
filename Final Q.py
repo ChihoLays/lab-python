c85e61cbc62b9f63420c269dce782b5175e98020
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2020-12-03 00:02:17 +0700

    Update Final Q.py

--- Final Q.py
+++ Final Q.py
@@ -34,3 +34,37 @@ def recur_find_route(start,routes,sum_dis = 0.0):
         return recur_find_route(start,routes,sum_dis)
 print(recur_find_route("a",routes))
 print(recur_find_route("b",routes))
+#Q4
+class EWallet:
+    def __init__(self,ca,name,max_val):
+        self.__current_amount = ca
+        self.owner = name
+        self.max_val = max_val
+    def put_in(self,money):
+        if (self.__current_amount+money) > self.max_val:
+            print("Sorry, but the amount will exceed the limit.")
+        else:
+            self.__current_amount += money
+            print("Successfully deposit money.")
+    def take_out(self,money):
+        if (self.__current_amount-money) < 0:
+            print("Sorry, your balance is not enough.")
+        else:
+            self.__current_amount -= money
+            print("Successfully withdraw money.")
+    def show_current_amount(self):
+        print(f"Balance: {self.__current_amount} ฿")
+#Q5
+class SecuredEWallet(EWallet):
+    def __init__(self,ca,name,max_val):
+        super().__init__(ca,name,max_val)
+        self.history = []
+    def put_in(self,money):
+        super().put_in(money)
+        self.history.append(f"Put in {money} ฿")
+    def take_out(self,money):
+        super().take_out(money)
+        self.history.append(f"Take out {money} ฿")
+    def show_history(self):
+        for log in self.history:
+            print(log)
